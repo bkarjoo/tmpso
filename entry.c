badffd43abb86c20b256b75e137be5afce5e8eac
Behrooz Karjoo <bkarjoo@gmail.com>
2019-10-29 10:19:57 -0400

    initial commit

---
+++ entry.c
@@ -0,0 +1,20 @@
+#include "../etc/functions.h"
+entry_order_type = LIMIT
+entry_order_side = SHORT
+entry_destination = CSFB
+entry_size = 100
+entry_tif = TIF_OPENING
+entry_tif_seconds =
+entry_order_limit =
+{
+
+}
+
+entry_aggregated_TIF = FALSE
+entry_calculate_limit_during_placement = FALSE
+entry_tif2 =
+entry_tif2_seconds =
+entry_order_stop =
+{
+
+}
